6a56bf05a8b07994aafdb96d7822d9982f76494c
MustafaBasar471 <mustafabasar471@gmail.com>
2022-06-25 23:47:20 +0300

    Todolist Update

--- Todolist/app.js
+++ Todolist/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + '/date.js');
+const mongoose = require('mongoose');
+const { ObjectId } = require("mongodb")
+const db = "mongodb+srv://<username>:<password>@mydatabase.o4uztco.mongodb.net/?retryWrites=true&w=majority"
 
 const app = express();
 
@@ -8,35 +10,71 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"))
 
-let items = ["Todo1", "Todo2", "Todo3"];
-let workitems = [];
+try {
+    mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+    console.log('DB status : OK')
+} catch (err) {
+    console.log(err)
+}
+
+const todoSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", todoSchema);
+
+const item1 = new Item({
+    name: "Welcome to your todolist",
+});
+
+const item2 = new Item({
+    name: "Hit the + button to aff a new item",
+});
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item",
+});
+
+const defaultItems = [item1, item2, item3];
 
 app.get('/', (req, res) => {
-    let day = date()
-    res.render("list", {listTitle: day, newListItems: items})
+    Item.find({}, (err, foundItems) => {
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems, (err) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log("Successfully saved defaultItems ")
+                }
+            });
+            res.redirect('/')
+        } else {
+            res.render("list", {listTitle: "Today", newListItems: foundItems})
+        }      
+    });
 });
 
 app.post("/", (req, res) => {
-    let item = req.body.newItem
-    if (req.body.list === "Work") {
-        workitems.push(item);
-        res.redirect('/work')
-    } else {
-        items.push(item)
-        res.redirect("/");
-    }
+    const itemName = req.body.newItem;
+    const item = new Item({
+        name: itemName,
+    });
+    item.save()
+    res.redirect('/')
 });
 
-app.get('/work', (req, res) => {
-    res.render("list", {listTitle: "Work List", newListItems: workitems})
-})
-
-app.post('/work', (req, res) => {
-    let item = req.body.newItem;
-    workitems.push(item);
-    res.redirect('/work')
-})
+app.post('/delete', (req, res) => {
+    const id = mongoose.Types.ObjectId(req.body.checkbox.trim());
+    Item.findByIdAndRemove(id, (err) => {
+        if (!err) {
+            console.log("Item Deleted")
+            res.redirect('/')
+        } else { 
+            console.log(err)
+        }
+    });
+});
 
 app.listen(3000, () => {
     console.log("Server started on port 3000")
-})
+});
